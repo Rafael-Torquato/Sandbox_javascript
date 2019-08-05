cdc0520ad1a9b216c7c7683ae45d91362d03babb
Rafael-Torquato <rafaelsoar@gmail.com>
2019-08-05 16:59:01 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sandbox_javascript
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="headermenu"
+  <h1>menu</h1>
+</div>
+
+<div class="navmenu">
+  <lu>
+   Balões
+    <li>Personagens</li>
+    <li> Filmes
+      Números
+      Símbolos
+    </li>
+    Contatos
+     <li>Instagram</li>
+     <li>Facebook</li>
+  </lu>
+</div>
+<div class="content">
+<p< teste </p>
+</div>
+`;
